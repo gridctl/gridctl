d7aa3dbf2edb350dd9e51bb97801274227571007
William Collins <26700607+wcollins@users.noreply.github.com>
2026-07-23 16:49:04 -0400

    chore: export new log atoms from barrel

--- web/src/components/log/index.ts
+++ web/src/components/log/index.ts
@@ -1,11 +1,18 @@
 export { LogLine } from './LogLine';
 export { LogsTab } from './LogsTab';
 export { LevelFilter } from './LevelFilter';
+export { LogFilterBar } from './LogFilterBar';
+export { LogStream } from './LogStream';
 export {
   type LogLevel,
   type ParsedLog,
+  type LogFilter,
   LOG_LEVELS,
   LEVEL_STYLES,
+  GATEWAY_LOG_SOURCE,
   parseLogEntry,
   formatTimestamp,
+  logSourceOf,
+  normalizeLogSourceParam,
+  filterParsedLogs,
 } from './logTypes';
